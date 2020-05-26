61d07a262a0ee8add172cbdc2b7d4e8764402f3a
WQDev <weiqiang2265@163.com>
2020-05-26 14:53:14 +0800

    去掉WQCTMediator依赖

--- WQCTMediator_Extension/Classes/CTMediator+MessageModule.swift
+++ WQCTMediator_Extension/Classes/CTMediator+MessageModule.swift
@@ -6,27 +6,27 @@
 //
 
 import UIKit
-import WQCTMediator
-
-struct TargetMessageModule {
-    static let name: String = "MessageModule"
-}
-
-struct ActionMessageModule {
-    
-    static let mainViewController = "mainViewController"
-}
-
-extension CTMediator {
-    
-    public func ny_messageModuleViewController(userId: String) -> UIViewController {
-        
-        let param: [String : Any] =  [kCTMediatorParamsKeySwiftTargetModuleName: "MessageModule",
-                                      "userId": userId]
-        let vc = self.performTarget(TargetMessageModule.name,
-                           action: ActionMessageModule.mainViewController,
-                           params: param,
-                           shouldCacheTarget: false) as! UIViewController
-        return vc
-    }
-}
+//import WQCTMediator
+//
+//struct TargetMessageModule {
+//    static let name: String = "MessageModule"
+//}
+//
+//struct ActionMessageModule {
+//    
+//    static let mainViewController = "mainViewController"
+//}
+//
+//extension CTMediator {
+//    
+//    public func ny_messageModuleViewController(userId: String) -> UIViewController {
+//        
+//        let param: [String : Any] =  [kCTMediatorParamsKeySwiftTargetModuleName: "MessageModule",
+//                                      "userId": userId]
+//        let vc = self.performTarget(TargetMessageModule.name,
+//                           action: ActionMessageModule.mainViewController,
+//                           params: param,
+//                           shouldCacheTarget: false) as! UIViewController
+//        return vc
+//    }
+//}
