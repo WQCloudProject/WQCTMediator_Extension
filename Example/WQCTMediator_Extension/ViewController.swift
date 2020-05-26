c24994dbadb2e4592e3368039aabb1243e22d032
WQDev <weiqiang2265@163.com>
2020-05-26 14:22:34 +0800

    工程提交

---
+++ Example/WQCTMediator_Extension/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WQCTMediator_Extension
+//
+//  Created by weiqiang2265@163.com on 05/26/2020.
+//  Copyright (c) 2020 weiqiang2265@163.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ WQCTMediator_Extension/Classes/CTMediator+MessageModule.swift
@@ -0,0 +1,32 @@
+//
+//  CTMediator+MessageModule.swift
+//  Pods-WQCTMediator_Extension_Example
+//
+//  Created by chenweiqiang on 2020/5/26.
+//
+
+import UIKit
+import WQCTMediator
+
+struct TargetMessageModule {
+    static let name: String = "MessageModule"
+}
+
+struct ActionMessageModule {
+    
+    static let mainViewController = "mainViewController"
+}
+
+extension CTMediator {
+    
+    public func ny_messageModuleViewController(userId: String) -> UIViewController {
+        
+        let param: [String : Any] =  [kCTMediatorParamsKeySwiftTargetModuleName: "MessageModule",
+                                      "userId": userId]
+        let vc = self.performTarget(TargetMessageModule.name,
+                           action: ActionMessageModule.mainViewController,
+                           params: param,
+                           shouldCacheTarget: false) as! UIViewController
+        return vc
+    }
+}
